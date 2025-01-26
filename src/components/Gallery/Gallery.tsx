ada466b8c4dc2bc08b99b874121df5a2033cf808
Dong-Young Kim <31337.persona@gmail.com>
2025-01-27 04:48:39 +0900

    feat: add `Gallery` and `GalleryItem` components

---
+++ src/components/Gallery/Gallery.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import type { GalleryItemProps } from "./GalleryItem";
+import GalleryItem from "./GalleryItem";
+
+interface GalleryProps {
+  items: GalleryItemProps[];
+}
+
+const Gallery: React.FC<GalleryProps> = ({ items }) => {
+  return (
+    <div className="grid grid-cols-4 gap-4">
+      {items.map((item) => (
+        // REVIEW: 서버에서 반환되는 데이터면 title 대신 사용할 수 있는 pk가 있을 수도?
+        <GalleryItem key={item.title} {...item} />
+      ))}
+    </div>
+  );
+};
+
+export default Gallery;
