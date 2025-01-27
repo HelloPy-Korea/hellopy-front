32239e58ed80b0c82264323824f6bcf24335af91
Dong-Young Kim <31337.persona@gmail.com>
2025-01-27 18:50:08 +0900

    fix: adjust spacing in `Gallery` component and update image size in `GalleryItem`

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -8,7 +8,7 @@ interface GalleryProps {
 
 const Gallery: React.FC<GalleryProps> = ({ items }) => {
   return (
-    <div className="grid grid-cols-4 gap-4">
+    <div className="grid grid-cols-4 gap-[31px]">
       {items.map((item) => (
         // REVIEW: 서버에서 반환되는 데이터면 title 대신 사용할 수 있는 pk가 있을 수도?
         <GalleryItem key={item.title} {...item} />
--- src/components/Gallery/GalleryItem.tsx
+++ src/components/Gallery/GalleryItem.tsx
@@ -12,7 +12,7 @@ export interface GalleryItemProps {
 const GalleryItem: React.FC<GalleryItemProps> = ({ image, title, description }) => {
   return (
     <div className="flex flex-col">
-      <img className="rounded-xl" src={image.url} alt={image.alt} />
+      <img className="h-[350px] rounded-xl object-cover" src={image.url} alt={image.alt} />
       <div className="flex flex-col mt-2">
         <h3 className="text-[20px] font-semibold">{title}</h3>
         <p className="text-[16px] font-light">{description}</p>
