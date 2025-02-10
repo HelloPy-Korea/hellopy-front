3e3ef75b57b5a54df1d8c04b00ad8a1d75835394
inh3011 <inh3011@gmail.com>
2025-02-10 15:13:12 +0900

    feat: editor component 추가

---
+++ src/components/QuillRichTextEditor.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css'; // Quill의 기본 스타일을 import 합니다.
+
+const RichTextEditor: React.FC = () => {
+    const [value, setValue] = useState<string>('');
+
+    const handleChange = (content: string) => {
+        setValue(content);
+    };
+
+    return (
+        <div className="my-4">
+            <ReactQuill theme="snow" value={value} onChange={handleChange} />
+        </div>
+    );
+};
+
+export default RichTextEditor;
