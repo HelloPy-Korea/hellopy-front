5781cdc00ae137716f04a5c9d7621da37a2b472e
seoinah <ireh2650@gmail.com>
2025-02-24 00:20:38 +0900

    fix: about md css 수정

--- src/pages/about/md/index.tsx
+++ src/pages/about/md/index.tsx
@@ -25,22 +25,20 @@ const photos = Array.from({ length: 7 }, (_, index) => ({
 const AboutMd = () => {
     return (
         <div className="flex flex-col align-center ">
-            <div className="w-full relative">
-                <HeaderBanner
-                    backgroundImage={BackImg}
-                    title="HelloPY MD"
-                    description={`저희 대표 캐릭터 파이몬으로 만든 귀여운 스티커들과 키링들..<br/>앞으로 더 많아질 굿즈들을 기대해 주세요!!`}
-                >
-                    <Breadcrumb children={[<a href="/">Home</a>, <a href="/about">ABOUT</a>, "MD"]}/>
-                </HeaderBanner>
-            </div>
+            <HeaderBanner
+                backgroundImage={BackImg}
+                title="HelloPY MD"
+                description={`저희 대표 캐릭터 파이몬으로 만든 귀여운 스티커들과 키링들..<br/>앞으로 더 많아질 굿즈들을 기대해 주세요!!`}
+            >
+                <Breadcrumb children={[<a href="/">Home</a>, <a href="/about">ABOUT</a>, "MD"]}/>
+            </HeaderBanner>
 
             <AboutSection>
                 <AboutSectionTitle title={"MD 한눈에 보기"} subtitle={"MD Overview"} description={[]}/>
                 <Gallery items={photos}/>
             </AboutSection>
 
-            <div className="w-full relative">
+            <div className="w-[1920px] relative">
                 <img src={BanngerImg} className="w-full h-auto object-cover" alt="Intro Shape"/>
             </div>
         </div>
