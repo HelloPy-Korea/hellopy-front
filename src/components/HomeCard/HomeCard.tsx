6c82e9c3cf3d23c02f984cdc0c7e45f32f42d17b
ireh.s <ireh.s@kakaoenterprise.com>
2025-03-11 17:11:51 +0900

    fix: merge 에러 수정

--- src/components/HomeCard/HomeCard.tsx
+++ src/components/HomeCard/HomeCard.tsx
@@ -1,4 +1,4 @@
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 interface HomeCardProps {
   backgroundImage?: string;
@@ -11,25 +11,21 @@ interface HomeCardProps {
 }
 
 export const HomeCard = ({
-    backgroundImage,
-    title,
-    subtitle,
-    description,
-    buttonText,
-    textPosition = "left",
-    buttonUrl,
+  backgroundImage,
+  title,
+  subtitle,
+  description,
+  buttonText,
+  textPosition = "left",
+  buttonUrl,
 }: HomeCardProps) => {
+  const navigate = useNavigate();
 
-
-    const navigate = useNavigate();
-
-    const redirectToUrl = () => {
-        if(!buttonUrl || buttonUrl.trim().length===0) {
-            return;
-        }
-        navigate(buttonUrl)
+  const redirectToUrl = () => {
+    if (!buttonUrl || buttonUrl.trim().length === 0) {
+      return;
     }
-    //navigate(buttonUrl)
+    navigate(buttonUrl);
   };
 
   return (
@@ -39,31 +35,35 @@ export const HomeCard = ({
     >
       {/* <div className="absolute inset-0 bg-black/10"></div>*/}
 
-            <div
-                className={`absolute top-[121px] flex flex-col gap-40 text-black
-                    ${textPosition === "left" ? "left-[322px]" : "right-[322px] text-right"}`}
-            >
-                <div className="flex flex-col gap-5">
-                    <div className="flex flex-col gap-1">
-                        {subtitle &&
-									        <span className="text-xl font-semibold text-hellopy-purple-200 leading-[30px]">{subtitle}</span>}
-                        <h1 className="text-[32px] font-semibold">{title}</h1>
-                    </div>
-                    {description && <p className="text-lg font-light leading-[27px]"
-						                           dangerouslySetInnerHTML={{__html: description}}/>}
-                </div>
-
-
-                {buttonText && <button
-	                className={`py-1.5 border-b border-black text-base font-medium leading-tight 
-                        ${textPosition === "left" ? "self-start" : "self-end"}`}
-					        onClick={redirectToUrl}
-				        >
-                    {buttonText}
-				        </button>}
-            </div>
+      <div
+        className={`absolute top-[121px] flex flex-col gap-40 text-black ${textPosition === "left" ? "left-[322px]" : "right-[322px] text-right"}`}
+      >
+        <div className="flex flex-col gap-5">
+          <div className="flex flex-col gap-1">
+            {subtitle && (
+              <span className="text-xl font-semibold leading-[30px] text-hellopy-purple-200">
+                {subtitle}
+              </span>
+            )}
+            <h1 className="text-[32px] font-semibold">{title}</h1>
+          </div>
+          {description && (
+            <p
+              className="text-lg font-light leading-[27px]"
+              dangerouslySetInnerHTML={{ __html: description }}
+            />
+          )}
         </div>
 
-        
+        {buttonText && (
+          <button
+            className={`border-b border-black py-1.5 text-base font-medium leading-tight ${textPosition === "left" ? "self-start" : "self-end"}`}
+            onClick={redirectToUrl}
+          >
+            {buttonText}
+          </button>
+        )}
+      </div>
+    </div>
   );
 };
