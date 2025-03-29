dd3ea5bf08488bfb65fc2f793ef9a63c4e4a37c8
seoinah <ireh2650@gmail.com>
2025-03-29 10:32:57 +0900

    fix: edit coc header

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -5,7 +5,11 @@ import Footer from "@/components/Footer/Footer.tsx";
 const Layout = () => {
   const location = useLocation();
   const isYellow = (): boolean => {
-    return location.pathname === "/about" || location.pathname === "/about/md";
+    return (
+      location.pathname === "/about" ||
+      location.pathname === "/about/md" ||
+      location.pathname === "/coc"
+    );
   };
   return (
     <div>
